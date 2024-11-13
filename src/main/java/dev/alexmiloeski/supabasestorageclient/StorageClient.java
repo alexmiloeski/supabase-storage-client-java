dca404e36a6b4ffee832169050b3bb9c404ac4ae
alexmiloeski <a.miloeski@gmail.com>
2024-11-13 12:44:10 +0100

    feat(client): implement some StorageClient methods for reading buckets and files

--- src/main/java/dev/alexmiloeski/supabasestorageclient/StorageClient.java
+++ src/main/java/dev/alexmiloeski/supabasestorageclient/StorageClient.java
@@ -1,5 +1,10 @@
 package dev.alexmiloeski.supabasestorageclient;
 
+import dev.alexmiloeski.supabasestorageclient.model.Bucket;
+import dev.alexmiloeski.supabasestorageclient.model.FileObject;
+
+import java.util.List;
+
 public class StorageClient {
     private final String apiUrl;
     private final String apiKey;
@@ -7,5 +12,52 @@ public class StorageClient {
     public StorageClient(String projectId, String apiKey) {
         this.apiUrl = "https://" + projectId + ".supabase.co";
         this.apiKey = apiKey;
+    }
+
+    public List<Bucket> listBuckets() {
+        // GET url/dev/v1/bucket
+        String body = newRequest()
+                .bucket()
+                .make();
+        return Mapper.toBuckets(body);
+    }
+
+    public Bucket getBucket(final String bucketId) {
+        // GET url/dev/v1/bucket/some-bucket
+        String body = newRequest()
+                .bucket()
+                .path(bucketId)
+                .make();
+        return Mapper.toBucket(body);
+    }
+
+    public List<FileObject> listFilesInBucket(final String bucketId, final String folderId) {
+        // POST url/dev/v1/object/list/test-bucket-1
+        String body = newRequest()
+                .object()
+                .path("/list/" + bucketId)
+                .post("""
+                        {"limit":100,"offset":0,"sortBy":{"column":"name","order":"asc"},"prefix":"%s"}"""
+                        .formatted(folderId))
+                .jsonContent()
+                .make();
+        return Mapper.toObjects(body);
+    }
+
+    public List<FileObject> listFilesInBucket(final String bucketId) {
+        return listFilesInBucket(bucketId, "");
+    }
+
+    public String downloadFile(final String bucketId, final String fileId) {
+        // GET url/dev/v1/object/some-bucket/some-file
+        return newRequest()
+                .object()
+                .path(bucketId + "/" + fileId)
+                .make();
+//        return Mapper.toBucket(response.body());
+    }
+
+    private RequestMaker newRequest() {
+        return new RequestMaker(apiUrl, apiKey);
     }
 }
