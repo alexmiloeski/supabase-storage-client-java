87c9736ad1aeb888c4dcfbe2f82fcec846d327e6
alexmiloeski <a.miloeski@gmail.com>
2024-11-13 12:43:07 +0100

    feat(client): implement utility class for mapping responses to model

---
+++ src/main/java/dev/alexmiloeski/supabasestorageclient/Mapper.java
@@ -0,0 +1,47 @@
+package dev.alexmiloeski.supabasestorageclient;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.alexmiloeski.supabasestorageclient.model.Bucket;
+import dev.alexmiloeski.supabasestorageclient.model.FileObject;
+
+import java.util.List;
+
+class Mapper {
+    static final ObjectMapper mapper = new ObjectMapper();
+
+    static Bucket toBucket(String string) {
+        if (string == null) return null;
+        try {
+            return mapper.readValue(string, Bucket.class);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            System.out.println("COULDN'T MAP YO!");
+            return null;
+        }
+    }
+
+    static List<Bucket> toBuckets(String string) {
+        if (string == null) return null;
+        try {
+            return mapper.readValue(string, new TypeReference<>() {
+            });
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            System.out.println("COULDN'T MAP YO!");
+            return null;
+        }
+    }
+
+    static List<FileObject> toObjects(String string) {
+        if (string == null) return null;
+        try {
+            return mapper.readValue(string, new TypeReference<>() {});
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            System.out.println("COULDN'T MAP YO!");
+            return null;
+        }
+    }
+}
