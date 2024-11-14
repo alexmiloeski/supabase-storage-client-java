19aa4e4559b07ec7ac430e6405809eb1da9e4627
alexmiloeski <a.miloeski@gmail.com>
2024-11-14 11:17:30 +0100

    test(client): add tests for creating bucket and deleting bucket (wip names)

--- src/test/java/dev/alexmiloeski/supabasestorageclient/StorageClientTest.java
+++ src/test/java/dev/alexmiloeski/supabasestorageclient/StorageClientTest.java
@@ -1,9 +1,14 @@
 package dev.alexmiloeski.supabasestorageclient;
 
 import io.github.cdimascio.dotenv.Dotenv;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.*;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class StorageClientTest {
+
+    final String testBucketId = "test-bucket-4";
 
     StorageClient storageClient;
 
@@ -13,5 +18,23 @@ class StorageClientTest {
         String projectId = dotenv.get("SUPABASE_STORAGE_PROJECT_ID");
         String apiKey = dotenv.get("SUPABASE_STORAGE_API_KEY");
         storageClient = new StorageClient(projectId, apiKey);
+    }
+
+    @Test
+    @Order(10)
+    @Disabled
+    void createBucket() {
+        String testBucketIdRes = storageClient.createBucket(testBucketId, testBucketId, false, null, null);
+        assertEquals(testBucketId, testBucketIdRes);
+    }
+
+    @Test
+    @Order(50)
+    @Disabled
+    void deleteBucket() throws InterruptedException {
+        Thread.sleep(100);
+        String message = storageClient.deleteBucket(testBucketId);
+        assertNotNull(message);
+        assertFalse(message.isEmpty());
     }
 }
