56cde942d68f9acceaaba740b42c757aecb8d4f7
alexmiloeski <a.miloeski@gmail.com>
2024-11-16 09:25:34 +0100

    feat(model): throw when all fields are missing in ErrorResponse json

--- src/main/java/dev/alexmiloeski/supabasestorageclient/model/responses/ErrorResponse.java
+++ src/main/java/dev/alexmiloeski/supabasestorageclient/model/responses/ErrorResponse.java
@@ -1,6 +1,15 @@
 package dev.alexmiloeski.supabasestorageclient.model.responses;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record ErrorResponse(String statusCode, String error, String message) {}
+public record ErrorResponse(String statusCode, String error, String message) {
+    public static final String MISSING_FIELDS_MESSAGE = "Missing required fields";
+    @JsonCreator
+    public ErrorResponse {
+        if (statusCode == null && error == null && message == null) {
+            throw new IllegalArgumentException(MISSING_FIELDS_MESSAGE);
+        }
+    }
+}
