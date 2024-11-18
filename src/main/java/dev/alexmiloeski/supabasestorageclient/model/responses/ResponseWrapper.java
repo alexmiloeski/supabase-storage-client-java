2bfebad19cffb8816b9962e68e4ff04d9844e4bd
alexmiloeski <a.miloeski@gmail.com>
2024-11-18 19:58:19 +0100

    refactor(model): add response wrapper boolean utility methods that say if there's a body or error or exception

--- src/main/java/dev/alexmiloeski/supabasestorageclient/model/responses/ResponseWrapper.java
+++ src/main/java/dev/alexmiloeski/supabasestorageclient/model/responses/ResponseWrapper.java
@@ -1,4 +1,15 @@
 package dev.alexmiloeski.supabasestorageclient.model.responses;
 
 public record ResponseWrapper<T>(T body, ErrorResponse errorResponse, String exception) {
+    public boolean hasBody() {
+        return body != null;
+    }
+
+    public boolean hasError() {
+        return errorResponse != null;
+    }
+
+    public boolean hasException() {
+        return exception != null;
+    }
 }
