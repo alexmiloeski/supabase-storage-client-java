5e8d14bbe667d62ebff1967fadce0def5a69a02d
alexmiloeski <a.miloeski@gmail.com>
2024-11-28 11:12:03 +0100

    test(client-unit): create test version of StorageClient that takes a mock RequestMaker

--- src/test/java/dev/alexmiloeski/supabasestorageclient/StorageClientUnitTest.java
+++ src/test/java/dev/alexmiloeski/supabasestorageclient/StorageClientUnitTest.java
@@ -2,11 +2,31 @@ package dev.alexmiloeski.supabasestorageclient;
 
 import org.junit.jupiter.api.BeforeEach;
 
+import static org.mockito.Mockito.mock;
+
 class StorageClientUnitTest {
+
+    RequestMaker mockRequestMaker;
     StorageClient storageClient;
 
     @BeforeEach
     void setUp() {
-        storageClient = new StorageClient("", "");
+        mockRequestMaker = mock(RequestMaker.class);
+        storageClient = new TestStorageClient("", "", mockRequestMaker);
+    }
+
+    private static class TestStorageClient extends StorageClient {
+
+        private final RequestMaker mockRequestMaker;
+
+        public TestStorageClient(String projectId, String apiKey, RequestMaker mockRequestMaker) {
+            super(projectId, apiKey);
+            this.mockRequestMaker = mockRequestMaker;
+        }
+
+        @Override
+        protected RequestMaker newRequest() {
+            return mockRequestMaker;
+        }
     }
 }
