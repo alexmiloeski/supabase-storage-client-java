72eb96fcc68d58396b3e02e114d7615adde62806
alexmiloeski <a.miloeski@gmail.com>
2024-11-28 17:55:20 +0100

    test(client-integration): add healthCheck tests

--- src/test/java/dev/alexmiloeski/supabasestorageclient/StorageClientIntegrationTest.java
+++ src/test/java/dev/alexmiloeski/supabasestorageclient/StorageClientIntegrationTest.java
@@ -3,6 +3,12 @@ package dev.alexmiloeski.supabasestorageclient;
 import com.github.tomakehurst.wiremock.junit5.WireMockRuntimeInfo;
 import com.github.tomakehurst.wiremock.junit5.WireMockTest;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static dev.alexmiloeski.supabasestorageclient.Arrange.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @WireMockTest
 public class StorageClientIntegrationTest {
@@ -15,6 +21,30 @@ public class StorageClientIntegrationTest {
     void setUp(WireMockRuntimeInfo wmRuntimeInfo) {
         final int port = wmRuntimeInfo.getHttpPort();
         storageClient = new TestStorageClient("any", TEST_API_KEY, port);
+    }
+
+    @Test
+    void healthCheckReturnsTrue() {
+        stubFor(get("/storage/v1/health").willReturn(ok().withBody(HEALTHY_JSON)));
+
+        boolean isHealthy = storageClient.isHealthy();
+        assertTrue(isHealthy);
+    }
+
+    @Test
+    void healthCheckReturnsFalseWhenServerSaysFalse() {
+        stubFor(get("/storage/v1/health").willReturn(ok().withBody(UNHEALTHY_JSON)));
+
+        boolean isHealthy = storageClient.isHealthy();
+        assertFalse(isHealthy);
+    }
+
+    @Test
+    void healthCheckReturnsFalseWhenServerErrors() {
+        stubFor(get("/storage/v1/health").willReturn(serverError()));
+
+        boolean isHealthy = storageClient.isHealthy();
+        assertFalse(isHealthy);
     }
 
     private static class TestStorageClient extends StorageClient {
